85953ba670e2981a41b1dd6f33997e1efd74c383
Jignesh Patil <patiljignesh@gmail.com>
2022-08-18 17:11:52 +0200

    Add Custom TableViewCell.

---
+++ SomeNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  SomeNews
+//
+//  Created by Jigneshkumar Patil on 2022/08/18.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
